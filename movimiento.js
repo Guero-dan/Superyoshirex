d7b6e4e8ec3d6dbdfe1f45f6c7a011a7d5306aa4
Guero-dan <111529313+Guero-dan@users.noreply.github.com>
2024-04-13 17:11:56 -0600

    Add files via upload

--- movimiento.js
+++ movimiento.js
@@ -3,7 +3,10 @@ function preload(){
     f=loadImage("./ground/atras/fondo.png")
     F=loadImage("./ground/atras/fondo-1.png.png")
     i=loadImage("./ground/atras/fondo-1.png.png")
+    M=loadImage("./Sprites/M0.png")
+    CY=loadImage("./Sprites/y0-1.png.png")
 }
+var vida=5
 function setup(){
     createCanvas(windowWidth, windowHeight)
     fondo=createSprite(917,height-400)
@@ -18,25 +21,54 @@ function setup(){
     I.scale=3
     yoshi=createSprite(width/2, height*0.6)
     yoshi.addAnimation("camina", camina)
+    frameRate(60)
+    man=createGroup()
+    cad=createGroup()
+    for (bar=1; bar<=vida; bar++){
+        cy=createSprite(50*bar,50)
+        cy.addImage(CY)
+        cad.add(cy)
+    }
+}
+function EC(yoshi, manzana){
+    manzana.destroy()
+    vida++
+    cy=createSprite(50*vida,50)
+    cy.addImage(CY)
+    cad.add(cy)
 }
 function draw(){
     background("deepSkyBlue")
     drawSprites()
+    if (frameCount%60==0){
+        manzana=createSprite(yoshi.x+random(50, 200), yoshi.y)
+        manzana.addImage(M)
+        man.add(manzana)
+    }
+    //text(vida)
+    //tiempo vidas final=18000
+    yoshi.overlap(man, EC)
     if (keyDown(RIGHT_ARROW)){
         yoshi.mirrorX(1)
-        yoshi.x+=50000
+        yoshi.x+=50
         camera.position.x=yoshi.x
+        cad.forEach(cv=>{
+            cv.x=cv.x+50
+        })
     }
     if (keyDown(LEFT_ARROW)){
         yoshi.mirrorX(-1)
         yoshi.x-=10
         camera.position.x=yoshi.x
+        cad.forEach(cv=>{
+            cv.x=cv.x-10
+        })
     }
-    if (camera.position.x-2000>Fondo.x){
-        Fondo.x=Fondo.x+4320
+    if (camera.position.x-2800>Fondo.x){
+        Fondo.x=Fondo.x+4330
     }
-    if (camera.position.x-2000>I.x){
-        I.x=I.x+4320
+    if (camera.position.x-2800>I.x){
+        I.x=I.x+4330
     }
     
     }
